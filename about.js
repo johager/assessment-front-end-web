7bc415edc9300fa51608c219936f72ee8b42b36e
James Hager <james@avantiapplications.com>
2022-02-06 14:54:02 -0700

    "1) Mod neighborhood to have baseling a and span tags. 2) Clean up nav. 3) Mod profile flex stuff to use "box-sizing: border-box" so the square is a square.

--- about.js
+++ about.js
@@ -1,8 +1,8 @@
 console.log("hello world");
 
 function handleImgMouseover(event) {
-	alert('You moused-over the cat image.\n\nCongratulations on finding an Easter egg.')
-
+	alert(`You moused-over the cat image.\n\nCongratulations on finding an Easter egg.\n\nYou won't see this alert until you reload the page.`)
+	event.target.removeEventListener('mouseover', handleImgMouseover)
 }
 
 function handleSubmit(evt) {
